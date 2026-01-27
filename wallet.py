3d28d00c525bf57f19a60099fb9a4dbab082ccb7
Monster0506 <tjraklovits@gmail.com>
2026-01-27 07:27:58 -0500

    Wallet implementation

--- wallet.py
+++ wallet.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ed25519
+
 from structs import PrivateKey, PublicKey, Signature
 
 
@@ -9,9 +12,35 @@ class Wallet:
     private_key: PrivateKey
 
     @classmethod
-    def generate(cls) -> Wallet: ...
+    def generate(cls) -> Wallet:
+        privkey_obj = ed25519.Ed25519PrivateKey.generate()
+        priv_bytes = privkey_obj.private_bytes(
+            encoding=serialization.Encoding.Raw,
+            format=serialization.PrivateFormat.Raw,
+            encryption_algorithm=serialization.NoEncryption(),
+        )
+        pubkey_obj = privkey_obj.public_key()
+        pub_bytes = pubkey_obj.public_bytes(
+            encoding=serialization.Encoding.Raw,
+            format=serialization.PublicFormat.Raw,
+        )
+
+        return cls(public_key=PublicKey(pub_bytes), private_key=PrivateKey(priv_bytes))
 
     @classmethod
-    def from_private_key(cls, key: PrivateKey) -> Wallet: ...
+    def from_private_key(cls, key: PrivateKey) -> Wallet:
+        privkey_obj = ed25519.Ed25519PrivateKey.from_private_bytes(key.key_bytes)
+        pubkey_obj = privkey_obj.public_key()
+        pub_bytes = pubkey_obj.public_bytes(
+            encoding=serialization.Encoding.Raw,
+            format=serialization.PublicFormat.Raw,
+        )
 
-    def sign(self, data: bytes) -> Signature: ...
+        return cls(public_key=PublicKey(pub_bytes), private_key=key)
+
+    def sign(self, data: bytes) -> Signature:
+        privkey_obj = ed25519.Ed25519PrivateKey.from_private_bytes(
+            self.private_key.key_bytes
+        )
+        sig = privkey_obj.sign(data)
+        return Signature(sig)
